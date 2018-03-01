fa5bbe28106cc0efcb0e4e1f93f752766996869a
marcialwushu <marcialwushu@gmail.com>
2018-03-01 15:58:35 -0300

    listPage register fucntion create and toast menssage added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ export class MyApp {
     this.pages = [
       { title: 'Home', component: HomePage },
       { title: 'List', component: ListPage },
-      { title: 'Test', component: TestPage }
+      { title: 'StarWars', component: TestPage }
     ];
 
   }
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,39 +1,52 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { Header } from 'ionic-angular/components/toolbar/toolbar-header';
+
+import { ToastController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
+  private url:string = 'https://beer.symfonycasts.com.br/v1';
   selectedItem: any;
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  items = {
+    name: "", 
+    price: "", 
+    type: "", 
+    mark: ""
+  };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+  constructor(
+    public navCtrl: NavController,
+    private toastCtrl: ToastController, 
+    public navParams: NavParams,
+    public http: Http
+  ) {
+    
   }
 
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
-      item: item
-    });
+  saveItems(items){
+    let headers = new Headers();
+    headers.append('Content-Type', 'aplication/json');
+
+    let options = new RequestOptions({ headers: headers });
+
+    this.http.post(this.url + '/beers', items, options)
+        .map(res => { res.json() })
+        .subscribe(data => {
+          let toast = this.toastCtrl.create({
+            message: 'Added successfuly',
+            duration: 3000
+          });
+          toast.present();
+        });
   }
+  
 
   
 }
